1db85b4bb6684de30e9575dd0bd0b6173685b0b5
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-04-12 23:04:51 +0530

    Phase 3

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,15 +5,19 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display  = ('username', 'email', 'get_full_name', 'role', 'is_active')
+    list_display  = ('username', 'email', 'get_full_name', 'role', 'is_active', 'date_joined')
     list_filter   = ('role', 'is_active', 'is_staff')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    ordering      = ('username',)
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'phone')
+    ordering      = ('first_name', 'last_name')
 
     fieldsets = BaseUserAdmin.fieldsets + (
-        ('Role & Profile', {'fields': ('role',)}),
+        ('Role & Contact', {
+            'fields': ('role', 'phone', 'date_of_birth', 'address', 'profile_picture')
+        }),
     )
 
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ('Role', {'fields': ('role',)}),
+        ('Role & Contact', {
+            'fields': ('first_name', 'last_name', 'email', 'role', 'phone')
+        }),
     )
